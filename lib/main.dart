627754cef22fd4976748532238d23d1a91a0637c
jamebryanbelmoro <jbelmoro_21UR0116@psu.edu.ph>
2024-09-19 23:19:28 +0800

    finished numbers

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,16 @@ import 'dart:ui';
 
 import 'package:json_converter/cursive/capital/caps_a.dart';
 import 'package:json_converter/cursive/capital/small_a.dart';
+import 'package:json_converter/numbers/number_0.dart';
+import 'package:json_converter/numbers/number_1.dart';
+import 'package:json_converter/numbers/number_2.dart';
+import 'package:json_converter/numbers/number_3.dart';
+import 'package:json_converter/numbers/number_4.dart';
+import 'package:json_converter/numbers/number_5.dart';
+import 'package:json_converter/numbers/number_6.dart';
+import 'package:json_converter/numbers/number_7.dart';
+import 'package:json_converter/numbers/number_8.dart';
+import 'package:json_converter/numbers/number_9.dart';
 
 import 'package:json_converter/standard/capital/caps_a.dart';
 import 'package:json_converter/standard/capital/caps_b.dart';
@@ -32,7 +42,8 @@ void saveOffsetsToFile(Map<String, Map<String, List<Offset?>>> offsetsMap) {
   File('guide_points.json').writeAsStringSync(jsonString);
 }
 
-void mergeOffsetsToFile(Map<String, Map<String, List<Offset?>>> newOffsetsMap) {
+void mergeOffsetsToFile(Map<String, Map<String, List<Offset?>>> newOffsetsMap,
+    {bool update = false}) {
   File file = File('guide_points.json');
   Map<String, dynamic> existingJsonMap = {};
 
@@ -49,14 +60,35 @@ void mergeOffsetsToFile(Map<String, Map<String, List<Offset?>>> newOffsetsMap) {
       existingJsonMap[category] = {};
     }
 
-    letters.forEach((letter, offsets) {
+    letters.forEach((letter, newOffsets) {
       // Convert the new offsets to JSON format
-      List<Map<String, dynamic>?> jsonOffsets = offsets.map((offset) {
+      List<Map<String, dynamic>?> jsonNewOffsets = newOffsets.map((offset) {
         return offset != null ? {'dx': offset.dx, 'dy': offset.dy} : null;
       }).toList();
 
-      // Merge or overwrite the new offsets for the given letter
-      existingJsonMap[category][letter] = jsonOffsets;
+      // If the letter exists and update flag is true, merge the offsets
+      if (update && existingJsonMap[category].containsKey(letter)) {
+        List<dynamic> existingOffsets = existingJsonMap[category][letter];
+
+        // Update existing offsets with new ones
+        for (int i = 0; i < newOffsets.length; i++) {
+          if (i < existingOffsets.length) {
+            if (newOffsets[i] != null) {
+              existingOffsets[i] =
+                  jsonNewOffsets[i]; // Update only non-null points
+            }
+          } else {
+            existingOffsets.add(jsonNewOffsets[
+                i]); // Add new points if the index is out of bounds
+          }
+        }
+
+        // Save the updated offsets back into the map
+        existingJsonMap[category][letter] = existingOffsets;
+      } else {
+        // Merge or overwrite the new offsets for the given letter if not updating
+        existingJsonMap[category][letter] = jsonNewOffsets;
+      }
     });
   });
 
@@ -83,12 +115,21 @@ void main() {
 
 //PARA SA PAG DAGDAG NG LETTER {
   Map<String, Map<String, List<Offset?>>> newOffsetsMap = {
-    'standard': {
-      'E': letter_E,
-      'e': letter_e,
+    'number': {
+      '0': number_0,
+      '1': number_1,
+      '2': number_2,
+      '3': number_3,
+      '4': number_4,
+      '5': number_5,
+      '6': number_6,
+      '7': number_7,
+      '8': number_8,
+      '9': number_9,
     }
   };
 
+  //PAG UPDATE, LAGYAN MO NG PARAMETER NA TRUE
   mergeOffsetsToFile(newOffsetsMap);
   //}
 }
